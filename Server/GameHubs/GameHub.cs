1a655c93a8f08c11e070674deb1bd5de2c5b36de
Demuirgos <Ayman.Bouchareb@etu.uae.ac.ma>
2022-02-02 21:37:11 +0100

    added some notifications in game, and fixed some bug in a buggy way

--- Server/GameHubs/GameHub.cs
+++ Server/GameHubs/GameHub.cs
@@ -2,12 +2,13 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
 using Game.Architecture;
 using Game.Models;
+using System.Linq;
 
 namespace Game.Hubs;
 public class GameHub : Hub
 {
     public enum MessageType {
-        GetNotification,  GetUpdate,  GetMessage,
+        GetNotification,  GetUpdate,  GetMessage, GetIndicator,
         GetGameStarted, GetGameEnded, AlterState, GetState
     }
     private static Manager Engine = new Manager();
@@ -24,6 +25,17 @@ public class GameHub : Hub
                 )).ToList();
     public override async Task OnConnectedAsync() 
         => await base.OnConnectedAsync();
+    public override async Task OnDisconnectedAsync(Exception? _) {
+        await base.OnDisconnectedAsync(null);
+        var room = OpenRooms.Where(
+                                r => r.Members.Contains(Context.ConnectionId) && r.State == PreState.Started
+                            ).FirstOrDefault();
+        if(room is not null)
+            Engine.UpdateRoom(room.Id, new Message {
+                Action = ActionType.ForfeitTurn,
+                PlayerId = Context.ConnectionId
+            }).FreeRooms();
+    } 
     public async Task<string> GetId() 
         =>  Context.ConnectionId; 
     public async Task<List<RoomsData>> GetRooms() 
@@ -86,10 +98,30 @@ public class GameHub : Hub
     }
 
     public async Task AlterState(string roomId, Message action){
+        var SendGameIndicators = async (string playerId, int? card) => {
+            await Clients.Clients(playerId).SendAsync(
+                MessageType.GetIndicator.ToString(), new Indicator(1, "It your turn Now!"));
+            await Clients.Group(roomId).SendAsync(
+                MessageType.GetIndicator.ToString(), new Indicator(2, card is not null ? $"claimed card : {card}" : "No card claimed"));
+            if(action.Action == ActionType.QuestionCredibility)
+            {
+                string message1 = "", message2 = "";
+                if(action.PlayerId != playerId){
+                    (message1, message2) = ("You Lost, He told the truth!!", "He Lost!!");
+                } else if (action.PlayerId == playerId) {
+                    (message1, message2) = ("You're right, He Lied!!", "He was Right!!");
+                }
+                await Clients.Clients(playerId).SendAsync(
+                    MessageType.GetIndicator.ToString(), new Indicator(3, message1));
+                await Clients.GroupExcept(roomId, playerId).SendAsync(
+                    MessageType.GetIndicator.ToString(), new Indicator(4, message2));
+            }
+        };
         try{
-            Engine.UpdateRoom(roomId, action);
+            var state = Engine.UpdateRoom(roomId, action).Rooms[roomId].State;
             await Clients.Group(roomId).SendAsync(
                 MessageType.GetState.ToString(), Engine.Rooms[roomId]);
+            await SendGameIndicators(state.CurrentPlayer, state.ClaimedCard);
         } catch(Exception e) {
             await Clients.Clients(action.PlayerId).SendAsync(
                 MessageType.GetNotification.ToString(), e.Message);
@@ -107,23 +139,28 @@ public class GameHub : Hub
     }
     public async Task AddToGroup(string roomId, string userId)
     {
-        OpenRooms
-            .Where(r => r.Id != roomId || r.OwnerId != userId)
-            .ToList().ForEach(async room => {
-                await RemoveFromGroup(room.Id, userId);
-                Engine.QuitRoom(room.Id, userId );
-            });
-        
-        Engine.JoinRoom(roomId, new Player(userId, Users[userId]));
-        await Groups.AddToGroupAsync(userId, roomId);
-        await Clients.Client(userId).SendAsync(
-            MessageType.GetNotification.ToString(), 
-            $"You Joined Room {OpenRooms?.Find(room => room.Id == roomId)?.Name}");
-        await Clients.Group(roomId).SendAsync(
-            MessageType.GetNotification.ToString(), 
-            $"Player {Users[userId]} Joined!!");
-        await Clients.All.SendAsync(
-            MessageType.GetUpdate.ToString(), OpenRooms);
+        if(Users.ContainsKey(userId)) {
+            OpenRooms
+                .Where(r => r.Id != roomId || r.OwnerId != userId)
+                .ToList().ForEach(async room => {
+                    await RemoveFromGroup(room.Id, userId);
+                    Engine.QuitRoom(room.Id, userId );
+                });
+            
+            Engine.JoinRoom(roomId, new Player(userId, Users[userId]));
+            await Groups.AddToGroupAsync(userId, roomId);
+            await Clients.Client(userId).SendAsync(
+                MessageType.GetNotification.ToString(), 
+                $"You Joined Room {OpenRooms?.Find(room => room.Id == roomId)?.Name}");
+            await Clients.Group(roomId).SendAsync(
+                MessageType.GetNotification.ToString(), 
+                $"Player {Users[userId]} Joined!!");
+            await Clients.All.SendAsync(
+                MessageType.GetUpdate.ToString(), OpenRooms);
+        } else {
+            await Clients.Clients(userId).SendAsync(
+                MessageType.GetNotification.ToString(), "Create Account First");
+        }
     }
 
     public async Task RemoveFromGroup(string roomId, string userId)
